5749c52936237ed3460312bb9db2b48d6c7eace6
shapirov103 <shapirov@amazon.com>
2022-01-12 00:20:18 -0500

    example of leveraging csi driver for secrets

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,8 @@ import { ManagedPolicy } from '@aws-cdk/aws-iam';
 import { Construct } from '@aws-cdk/core';
 import * as ssp from '@aws-quickstart/ssp-amazon-eks';
 import { HelmAddOn, HelmAddOnProps, HelmAddOnUserProps } from '@aws-quickstart/ssp-amazon-eks/dist/addons/helm-addon';
+import { CsiSecrets, CsiSecretsProps } from '@aws-quickstart/ssp-amazon-eks/dist/addons/secrets-store/csi-driver-provider-aws-secrets';
+
 
 export interface MyFluentBitAddOnProps extends HelmAddOnUserProps {
     cloudWatchRegion: string
@@ -28,16 +30,31 @@ export class MyFluentBitAddOn extends HelmAddOn {
         this.options = this.props as MyFluentBitAddOnProps;
     }
 
-    deploy(clusterInfo: ssp.ClusterInfo): void | Promise<Construct> {
+    @ssp.utils.dependable(ssp.SecretsStoreAddOn.name)
+    deploy(clusterInfo: ssp.ClusterInfo): Promise<Construct> {
         const serviceAccountName = 'aws-for-fluent-bit-sa';
         const sa = clusterInfo.cluster.addServiceAccount('my-aws-for-fluent-bit-sa', {
             name: serviceAccountName,
             namespace: this.props.namespace
         });
 
+        const csiSecret: CsiSecretsProps = {
+            secretProvider: new ssp.LookupSecretsManagerSecretByName('MyAddOnLicenseKey'),
+            kubernetesSecret: {
+                secretName: 'my-addon-license',
+                data: [
+                    {
+                        key: 'licenseKey'
+                    }
+                ]
+            }
+        };
+
         // Cloud Map Full Access policy.
         const cloudWatchAgentPolicy = ManagedPolicy.fromAwsManagedPolicyName("CloudWatchAgentServerPolicy");
         sa.role.addManagedPolicy(cloudWatchAgentPolicy);
+
+        new CsiSecrets([csiSecret], sa).setupSecrets(clusterInfo);
         
         const chart = this.addHelmChart(clusterInfo, {
             serviceAccount: {
