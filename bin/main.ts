437235d15db635014837b7137c88c5c2b9b72658
shapirov103 <shapirov@amazon.com>
2022-01-27 11:38:41 -0500

    test with argo csi secrets

--- bin/main.ts
+++ bin/main.ts
@@ -9,9 +9,18 @@ ssp.EksBlueprint.builder()
     .addOns(new ssp.ClusterAutoScalerAddOn)
     .addOns(new ssp.addons.SSMAgentAddOn) // needed for AWS internal accounts only
     .addOns(new ssp.SecretsStoreAddOn) // requires to support CSI Secrets
-     .addOns(new MyFluentBitAddOn({
+    .addOns(new MyFluentBitAddOn({
          cloudWatchRegion: 'us-east-2',
          licenseKeySecret: 'my-addon-license',
          namespace: 'my-addon-namespace'
+     }))
+     .addOns(new ssp.addons.ArgoCDAddOn({
+         adminPasswordSecretName: 'my-argo-hash',
+         bootstrapRepo: {
+             repoUrl: 'git@github.com:aws-samples/ssp-eks-workloads.git',
+             path: 'envs/test',
+             credentialsSecretName: 'github-ssp-ssh',
+             credentialsType: 'SSH'
+         }
      }))
      .build(app, 'my-extension-test-blueprint');
